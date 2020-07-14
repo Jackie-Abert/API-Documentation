ab2c807c0c650e7737fa47f1541578482bad8ae5
Jackie Bishop <63313775+JackieBishop@users.noreply.github.com>
2020-07-13 21:50:51 -0400

    changes

--- index.js
+++ index.js
@@ -1,50 +1,39 @@
 'use strict';
 
-function displayResults(responseJson) {
-    console.log('displayResults ran');
-    $(responseJson).ready(function () {
-        $('.loading').addClass('hidden')
-    })
-    console.log(responseJson)
-    console.log(responseJson.length)
-    console.log(responseJson[0].owner.login)
-    let user = responseJson[0].owner.login
-    let userinfo = `
-        <h4>User:${user}</h4>
-        <h4>Repos: ${responseJson.length}</h4>
-    `
-    $('.js-results').append(userinfo)
-    for (let i = 0; i < responseJson.length; i++) {
-        $('.js-results').append(`
-        <li><h4>${responseJson[i].name}</h4>
-        <a href="${responseJson[i].html_url}">${responseJson[i].html_url}</a>
-        </li>
-        `)
-    }
-    $('.js-results').removeClass('hidden')
-
+function getRepos(name) {
+  console.log('get repos')
+    fetch('https://api.github.com/users/${user}/repos')
+    .then(response => response.json())
+    .then(responseJson => 
+      displayResults(responseJson))
+    .catch(error => alert('Something went wrong. Try again later.'));
 }
 
-function getRepos(username) {
-    console.log('getRepos ran');
-    const url = `https://api.github.com/users/${username}/repos`
-    console.log(url);
-    fetch(url)
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-            throw new Error(response.statusText);
-        })
-        .then(responseJson => displayResults(responseJson))
-        .catch(err => {
-            displayError(err.message);
-        });
+
+function displayResults(data) {
+    console.log(data);
+    $('#results').empty();
+    for (let i = 0; i < data.items.length; i++){
+
+      $('#results').append(
+        `<li>
+            <h3>data.items[i].name</h3>
+            <a href=data.items[i].url>data.items[i].url</a>
+        </li>`
+      )};
+   
+    $('#results').removeClass('hidden');
+  };
+
+  function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    const name = $('#name').val();
+    getRepos(name);
+  });
 }
-function watchForm() {
-    $('#js-form').submit(event => {
-        event.preventDefault();
-        console.log('watchForm ran');
-    });
-}
-$(watchForm);
+
+$(function() {
+  console.log('App loaded! Waiting for submit!');
+  watchForm();
+});
